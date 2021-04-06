bfc1975e70c5d29e7040ebfbac805448bc7cdd9d
MorganWilliamson <mwilliamson7695@gmail.com>
2021-04-06 18:20:56 -0500

    Functionality for adding items.

--- ts-todo/src/App.tsx
+++ ts-todo/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { TodoList } from "./components/TodoList";
+import { AddTodoForm } from "./components/AddTodoForm";
 import './App.css';
 
 const initialTodos: Todo[] = [
@@ -27,9 +28,19 @@ function App() {
       return todo;
     });
     setTodos(newTodo);
-  }
+  };
 
-  return <TodoList todos={todos} toggleTodo={toggleTodo} />
+  const addTodo: AddTodo = (text: string) => {
+    const newTodo = { text, completed: false};
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div className="App">
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </div>
+  )
 }
 
 export default App;
